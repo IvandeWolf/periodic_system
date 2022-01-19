4f08a1e70f9711439544314ebb79c0ed7fa1e671
IvandeWolf <ivan.dewolf1997@gmail.com>
2022-01-19 20:24:50 +0100

    Comments!

--- src/components/Element.vue
+++ src/components/Element.vue
@@ -1,6 +1,10 @@
 <template>
+  <!-- If there is an element for this grid show it. On click we will make it active. -->
   <div v-if="element" @click="active = !active" class="relative">
+    <!-- Gray background screen. -->
     <div v-show="active" class="fixed top-0 left-0 w-screen h-screen bg-gray-700 opacity-50 z-20" />
+
+    <!-- The cell -->
     <div class="relative border flex flex-col w-full h-full items-center justify-end cursor-pointer hover:scale-150 bg-gray-800 duration-200 hover:z-30" :class="[active ? 'scale-300 z-30 hover:scale-300' : 'hover:scale-125', selected ? (selected === element.category ? '' : 'opacity-30') : '']" :style="`color: ${color}; border-color: ${color}`">
       <div class="absolute top-0 left-0 flex p-0.5">
         <span class="text-xs leading-3">{{ element.number }}</span>
@@ -8,14 +12,20 @@
       <span class="text-3xl z-10">{{ element.symbol }}</span>
       <span class="text-xs z-10">{{ element.name }}</span>
       <span class="text-xs z-10">{{ element.mass }}</span>
+
+      <!-- Only show the electron ring numbers when cell is active (clicked). -->
       <div v-show="active" class="absolute top-0 right-0 flex flex-col items-end p-0.5">
         <span v-for="(n, i) of element.electronrings" :key="i" class="text-2xs leading-2">{{ n }}</span>
       </div>
 
+      <!-- Show a representation of the electron rings when cell is active (clicked). -->
       <div v-show="active" class="absolute top-0 left-0 flex p-0.5 w-full h-full z-0">
+        <!-- For each ring create an svg. -->
         <template v-for="(e, ind) of element.electronrings.slice().reverse()" :key="ind">
           <svg v-if="element.electronrings.length >= ind + 1" class="w-full h-full absolute animate-spin" :style="`animation: spin ${20 - (ind * 2)}s linear infinite;`" viewBox="0 0 1000 1000">
+            <!-- The ring. -->
             <circle cx="500" cy="500" :r="420 - (50 * ind)" stroke="#474747" stroke-width="5" fill="transparent" />
+            <!-- Small dots which are a representation of an electron placed evenly alongside the ring. -->
             <circle
               v-for="i in e"
               :key="i" r="15" stroke="transparent" fill="#474747"
@@ -27,6 +37,8 @@
       </div>
     </div>
   </div>
+
+  <!-- If there is no element for this grid, leave it empty. -->
   <div v-else></div>
 </template>
 
@@ -38,13 +50,13 @@ export default {
       type: Object,
       required: true
     },
-    colors: {
+    categories: {
       type: Object,
       required: true
     },
     selected: {
       type: String,
-      required: true
+      required: false
     }
   },
   data: () => ({
@@ -52,7 +64,7 @@ export default {
   }),
   computed: {
     color () {
-      return this.colors[this.element.category]
+      return this.categories[this.element.category]
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="w-screen h-screen bg-gray-800 p-28 relative">
+  <div class="w-screen h-screen bg-gray-800 p-28 relative overflow-hidden">
     <div class="absolute top-32 left-0 w-full flex justify-center">
       <div class="relative flex space-x-8 mx-auto">
-        <span @click="selected = selected && selected === c ? null : c" v-for="c of categories" :key="c" class="cursor-pointer opacity-80 hover:opacity-100" :style="`color: ${colors[c]}`">{{ c }}</span>
+        <span @click="selected = selected && selected === c ? null : c" v-for="c of Object.keys(categories)" :key="c" class="cursor-pointer opacity-80 hover:opacity-100" :style="`color: ${categories[c]}`">{{ c }}</span>
       </div>
     </div>
     <div class="w-full h-full grid grid-cols-9 grid-rows-5 gap-0.5">
       <div v-for="i in (height / 2) * (width / 2)" :key="i" class="grid grid-cols-2 grid-rows-2 gap-0.5">
-        <Element :selected="selected" :colors="colors" :element="getElement(((i - 1) * 2) % width, Math.floor(((i - 1) * 2) / width) * 2)?.element" />
-        <Element :selected="selected" :colors="colors" :element="getElement(((i - 1) * 2) % width + 1, Math.floor(((i - 1) * 2) / width) * 2)?.element" />
-        <Element :selected="selected" :colors="colors" :element="getElement(((i - 1) * 2) % width, Math.floor(((i - 1) * 2) / width) * 2 + 1)?.element" />
-        <Element :selected="selected" :colors="colors" :element="getElement(((i - 1) * 2) % width + 1, Math.floor(((i - 1) * 2) / width) * 2 + 1)?.element" />
+        <Element :selected="selected" :categories="categories" :element="getElement(((i - 1) * 2) % width, Math.floor(((i - 1) * 2) / width) * 2)?.element" />
+        <Element :selected="selected" :categories="categories" :element="getElement(((i - 1) * 2) % width + 1, Math.floor(((i - 1) * 2) / width) * 2)?.element" />
+        <Element :selected="selected" :categories="categories" :element="getElement(((i - 1) * 2) % width, Math.floor(((i - 1) * 2) / width) * 2 + 1)?.element" />
+        <Element :selected="selected" :categories="categories" :element="getElement(((i - 1) * 2) % width + 1, Math.floor(((i - 1) * 2) / width) * 2 + 1)?.element" />
       </div>
     </div>
   </div>
@@ -20,6 +20,18 @@
 import Element from '@/components/Element'
 
 const periodic_table = require('@/assets/periodic_table.json')
+const categories = {
+  'Alkali Metals': '#e7b960',
+  'Alkaline Earth Metals': '#cedc66',
+  'Lanthanide': '#d9779d',
+  'Aktinoids': '#b57eb1',
+  'Transition Metals': '#dd7771',
+  'Post-transition Metals': '#7fbecd',
+  'Metalloids': '#73e8bd',
+  'Other Nonmetals': '#79e775',
+  'Noble Gasses': '#7fa3ed',
+  'Unknown': '#c8c8c5'
+}
 
 export default {
   name: 'Home',
@@ -29,34 +41,11 @@ export default {
   data: () => ({
     width: 18,
     height: 10,
-    selected: null,
-    categories: [
-      'Alkali Metals',
-      'Alkaline Earth Metals',
-      'Lanthanide',
-      'Aktinoids',
-      'Transition Metals',
-      'Post-transition Metals',
-      'Metalloids',
-      'Other Nonmetals',
-      'Noble Gasses',
-      'Unknown'
-    ],
-    colors: {
-      'Alkali Metals': '#e7b960',
-      'Alkaline Earth Metals': '#cedc66',
-      'Lanthanide': '#d9779d',
-      'Aktinoids': '#b57eb1',
-      'Transition Metals': '#dd7771',
-      'Post-transition Metals': '#7fbecd',
-      'Metalloids': '#73e8bd',
-      'Other Nonmetals': '#79e775',
-      'Noble Gasses': '#7fa3ed',
-      'Unknown': '#c8c8c5'
-    }
+    selected: null
   }),
   setup () {
     return {
+      categories,
       periodic_table
     }
   },
